97217b7cc2910c610084f0bb9af4941841f94eb7
not-nullptr <needhelpwithrift@gmail.com>
2024-11-11 15:27:57 +0000

    fix: oops

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,22 +1,7 @@
 <script lang="ts">
-	import { converters } from "$lib/converters";
-	import type { Converter } from "$lib/converters/converter";
+	import Uploader from "$lib/components/visual/Uploader.svelte";
 
-	let file = $state<File>();
-	let to = "";
-
-	const convert = async (converter: Converter, file: File, to: string) => {
-		const buffer = await file.arrayBuffer();
-		const result = await converter.convert({
-			name: file.name.split(".").slice(0, -1).join("."),
-			buffer
-		}, to);
-		console.log(result);
-	}
+	let files = $state<FileList>();
 </script>
 
-<div class="flex flex-col">
-	<input type="file" onchange={e => file = (e.target as any).files[0]} />
-	<input type="text" placeholder="to" bind:value={to} />
-	<button onclick={() => convert(converters[0], file!, to)}>Go</button>
-</div>
+<Uploader bind:files />
