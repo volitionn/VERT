9d7af8df70da88bb999346b29279033e500490f4
not-nullptr <needhelpwithrift@gmail.com>
2024-11-12 17:29:36 +0000

    fix: lower padding

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -44,7 +44,7 @@
 
 <div
 	role="main"
-	class="w-full h-full flex items-center p-8 flex-col gap-16"
+	class="w-full h-full flex items-center p-2 flex-col gap-16"
 	ondragenter={maybeNavToHome}
 >
 	<div
