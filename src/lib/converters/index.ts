772d195ae6b5863049fb7c21a32a6a71f1af6330
JovannMC <jovannmc@femboyfurry.net>
2025-02-08 21:31:04 +0300

    remove vertd
    not yet ready

--- src/lib/converters/index.ts
+++ src/lib/converters/index.ts
@@ -1,9 +1,9 @@
 import { FFmpegConverter } from "./ffmpeg.svelte";
-import { VertdConverter } from "./vertd.svelte";
+//import { VertdConverter } from "./vertd.svelte";
 import { VipsConverter } from "./vips.svelte";
 
 export const converters = [
 	new VipsConverter(),
 	new FFmpegConverter(),
-	new VertdConverter(),
+	//new VertdConverter(),
 ];
